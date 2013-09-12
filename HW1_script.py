84c268f27c0b60e41fee960e7b10496374b34685
In Ok Jun <jioda85@gmail.com>
2013-09-12 13:45:33 -0500

    Update HW1_script.py

--- HW1_script.py
+++ HW1_script.py
@@ -1,11 +1,9 @@
-"""
-HW1_script: Script to read burl1 historical wind data
-================================================================================
-         
-by InOk Jun
-OCNG 658, Fall 2013
-"""
-
+# ==============================================================================     
+# HW1_script: Script to read burl1 historical wind data
+# ==============================================================================     
+# by InOk Jun
+# OCNG 658, Fall 2013
+# ==============================================================================     
 
 # import statement
 import numpy as np
@@ -17,10 +15,9 @@ f = open('burl1_2011.dat')
 
 # make an empty list
 dates = []
-dirct = []
-speed = []
 press = []
-
+wind_U = []
+wind_V = []
 
 # read data using "for" loop (readlines)              
 for line in f.readlines()[2:]:
@@ -35,47 +32,37 @@ for line in f.readlines()[2:]:
     hour   = int(data[3])
     minute = int(data[4])
     dates.append( datetime(year, month, day, hour, minute) )
-
-    # input wind data
-    dirct.append( float(data[5]) )
-    speed.append( float(data[6]) )
     
     # input pressure
     press.append( float(data[12]) )
-
-
+    
+    # converting meteorological convection to oceanographic convection of wind
+    wind_U.append( -float(data[6]) * np.sin(float(data[5]) * np.pi/180.) )
+    wind_V.append( -float(data[6]) * np.cos(float(data[5]) * np.pi/180.) )
+ 
+       
 # change data as array
 dates = np.array(dates)
-speed = np.array(speed)
-dirct = np.array(dirct)
 press = np.array(press)
-
-# converting meteorological convection to oceanographic convection of wind
-for i in range(len(dirct)):
-    if dirct[i] == 999  : dirct[i] = np.Nan
-    elif dirct[i] >= 180: dirct[i] = dirct[i] - 180
-    else:                 dirct[i] = dirct[i] + 180
-
-# calculate the wind vectors using speed & direction
-wind = - speed * (np.sin(dirct * np.pi/180.), np.cos(dirct * np.pi/180.))
+wind_U = np.array(wind_U)
+wind_V = np.array(wind_V)
 
 
 # print the results
 print 'dates        = ', dates
-print 'wind (east)  = ', wind[0]
-print 'wind (north) = ', wind[1]
+print 'wind (east)  = ', wind_U
+print 'wind (north) = ', wind_V
 print 'press        = ', press
-
 
 # plotting the results
 plt.figure()
-plt.plot(dates, wind[0], '-r')
+plt.plot(dates, wind_U, '-r')
 plt.xlabel('dates')
 plt.ylabel('east (m/s)')
 plt.show()
 
 plt.figure()
-plt.plot(dates, wind[1], '-b')
+plt.plot(dates, wind_V, '-b')
 plt.xlabel('dates')
 plt.ylabel('north (m/s)')
 plt.show()
@@ -85,11 +72,3 @@ plt.plot(dates, press, '-g')
 plt.xlabel('dates')
 plt.ylabel('pressure (hPa)')
 plt.show()
-
-
-
-
-# NOTE: alternative format
-#data = {'dates': np.array(dates), 'press': np.array(press)}
-#print data['dates']
-#print data['press']
